5044c671df8161f0bb41a3c3c7c4e39d1f28e25c
Biniyam Abera Tilahun <myrita099@gmail.com>
2026-07-15 13:45:35 +0300

    Fix comment formatting in app.js

--- secondChance-backend/app.js
+++ secondChance-backend/app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use('*', cors())
 const port = 3060
 
-// Connect to MongoDB; we just do this one time.
+// Connect to MongoDB; we just do this one time
 connectToDatabase().then(() => {
   pinoLogger.info('Connected to DB')
 })
